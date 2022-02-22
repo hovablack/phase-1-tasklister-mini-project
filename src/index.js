139e3edc418a0c7ead77ddf030f5420c64bab087
Hova Black <nevermind92@hotmail.com>
2022-02-21 21:42:14 -0600

    Partially done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('#create-task-form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addTask(e.target[0].value)
+    form.reset();
+  });
 });
+
+function addTask(task) {
+  const li = document.createElement('li');
+  li.textContent = `${task} `
+  document.querySelector('#tasks').appendChild(li);
+  const btn = document.createElement('button');
+  btn.addEventListener('click', doDelete);
+  btn.textContent = 'x'
+  li.appendChild(btn)
+}
+
+
+function doDelete(e) {
+  e.target.parentNode.remove()
+}
